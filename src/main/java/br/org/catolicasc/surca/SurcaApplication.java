db78d08c1ed61627e616fd6c77a8a38ba111d363
Rodrigo Gregori <spam@gregori.eti.br>
2019-08-29 17:21:04 -0300

    Initial commit

---
+++ src/main/java/br/org/catolicasc/surca/SurcaApplication.java
@@ -0,0 +1,13 @@
+package br.org.catolicasc.surca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurcaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurcaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/org/catolicasc/surca/SurcaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.org.catolicasc.surca;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SurcaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
