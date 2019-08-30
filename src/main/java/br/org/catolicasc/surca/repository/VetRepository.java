dc1e64180dac7a52da8e9a9aa08f35e3974a6a35
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-08-30 17:54:15 -0300

    Add findByUserName, findByCrmv, findByUserNameStartingWith, findByUserEmail function

--- src/main/java/br/org/catolicasc/surca/repository/VetRepository.java
+++ src/main/java/br/org/catolicasc/surca/repository/VetRepository.java
@@ -1,6 +1,8 @@
 package br.org.catolicasc.surca.repository;
 
 import br.org.catolicasc.surca.model.Vet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import javax.transaction.Transactional;
@@ -8,4 +10,8 @@ import javax.transaction.Transactional;
 public interface VetRepository extends PagingAndSortingRepository<Vet, Long> {
     @Transactional
     void removeByUserUserLevelId(Long id);
+    Page<Vet> findByUserName(Pageable pageable, String name);
+    Page<Vet> findByCrmv(Pageable pageable, String crmv);
+    Page<Vet> findByUserNameStartingWith(Pageable pageable, String name);
+    Page<Vet> findByUserEmail(Pageable pageable, String crmv);
 }
