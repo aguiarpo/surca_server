a409d1b7c2fcb62a43dfc8594ae189ea1d9e5f5d
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-08-30 15:42:05 -0300

    Add removeByUserUserLevelId function

--- src/main/java/br/org/catolicasc/surca/repository/VetRepository.java
+++ src/main/java/br/org/catolicasc/surca/repository/VetRepository.java
@@ -3,5 +3,9 @@ package br.org.catolicasc.surca.repository;
 import br.org.catolicasc.surca.model.Vet;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import javax.transaction.Transactional;
+
 public interface VetRepository extends PagingAndSortingRepository<Vet, Long> {
+    @Transactional
+    void removeByUserUserLevelId(Long id);
 }
