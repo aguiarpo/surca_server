e4ce95585d0d95c0b9d1c3a25a8c2f28fb1a3100
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-08-30 16:31:15 -0300

    Add findByName, findByNameStartingWith, findByEmail, findByUserLevelName functions

--- src/main/java/br/org/catolicasc/surca/repository/UserRepository.java
+++ src/main/java/br/org/catolicasc/surca/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package br.org.catolicasc.surca.repository;
 
 import br.org.catolicasc.surca.model.User;
+import br.org.catolicasc.surca.model.UserLevel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import javax.transaction.Transactional;
@@ -8,4 +11,8 @@ import javax.transaction.Transactional;
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     @Transactional
     void removeByUserLevelId(Long id);
+    Page<User> findByName(Pageable pageable, String name);
+    Page<User> findByNameStartingWith(Pageable pageable, String name);
+    Page<User> findByEmail(Pageable pageable, String name);
+    Page<User> findByUserLevelName(Pageable pageable, String name);
 }
