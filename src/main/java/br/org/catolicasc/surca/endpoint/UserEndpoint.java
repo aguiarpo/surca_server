0bba7817720440d83ca655bac9d1059243abde2b
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-08-30 16:30:05 -0300

    Add UserEndpoint Class

---
+++ src/main/java/br/org/catolicasc/surca/endpoint/UserEndpoint.java
@@ -0,0 +1,79 @@
+package br.org.catolicasc.surca.endpoint;
+
+import br.org.catolicasc.surca.model.User;
+import br.org.catolicasc.surca.repository.UserLevelRepository;
+import br.org.catolicasc.surca.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("v1")
+public class UserEndpoint {
+
+    private UserRepository userDao;
+
+    @Autowired
+    public UserEndpoint(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
+    @GetMapping(path = "/user/usuario")
+    public ResponseEntity<?> listAll(Pageable pageable){
+        return new ResponseEntity<>(userDao.findAll(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/usuario/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable("id") Long id){
+        Optional<User> user =  userDao.findById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/usuario/nome/{nome}")
+    public ResponseEntity<?> getUserName(@PathVariable("nome") String name, Pageable pageable){
+        Page<User> user = userDao.findByName(pageable, name);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/usuario/nome/like/{nome}")
+    public ResponseEntity<?> getUserNameLike(@PathVariable("nome") String name, Pageable pageable){
+        Page<User> user = userDao.findByNameStartingWith(pageable, name);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/usuario/email/{email}")
+    public ResponseEntity<?> getUserEmail(@PathVariable("email") String email, Pageable pageable){
+        Page<User> user = userDao.findByEmail(pageable, email);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/usuario/idNivel/{idNivel}")
+    public ResponseEntity<?> getUserIdNivel(@PathVariable("idNivel") String name, Pageable pageable){
+        Page<User> user = userDao.findByUserLevelName(pageable, name);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/veterinario/usuario")
+    public ResponseEntity<?> save(@Valid @RequestBody User user){
+        userDao.save(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/veterinario/usuario/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        userDao.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/admin/usuario")
+    public ResponseEntity<?> update(@Valid @RequestBody User user){
+        return new ResponseEntity<>(userDao.save(user), HttpStatus.OK);
+    }
+
+}
