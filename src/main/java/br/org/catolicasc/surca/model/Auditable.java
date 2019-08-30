6364e34740cb17e8921279c51a5dd43235b43ea7
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-08-30 16:10:22 -0300

    Add getters and setters

--- src/main/java/br/org/catolicasc/surca/model/Auditable.java
+++ src/main/java/br/org/catolicasc/surca/model/Auditable.java
@@ -22,4 +22,36 @@ class Auditable {
     String createdBy;
     private @LastModifiedBy
     String lastModifiedBy;
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
 }
