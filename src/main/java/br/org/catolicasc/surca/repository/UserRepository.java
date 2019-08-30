84f19ff422edd298a0bdf8c08479fe1ab5749062
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-08-30 15:42:13 -0300

    Add removeByUserLevelId function

--- src/main/java/br/org/catolicasc/surca/repository/UserRepository.java
+++ src/main/java/br/org/catolicasc/surca/repository/UserRepository.java
@@ -3,5 +3,9 @@ package br.org.catolicasc.surca.repository;
 import br.org.catolicasc.surca.model.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import javax.transaction.Transactional;
+
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+    @Transactional
+    void removeByUserLevelId(Long id);
 }
