09e7d592967723942352ae1b3dc705644589ac22
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-08-30 15:34:15 -0300

    Add findByName function

--- src/main/java/br/org/catolicasc/surca/repository/UserLevelRepository.java
+++ src/main/java/br/org/catolicasc/surca/repository/UserLevelRepository.java
@@ -1,7 +1,10 @@
 package br.org.catolicasc.surca.repository;
 
 import br.org.catolicasc.surca.model.UserLevel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface UserLevelRepository extends PagingAndSortingRepository<UserLevel, Long> {
+    Page<UserLevel> findByName(Pageable pageable, String name);
 }
