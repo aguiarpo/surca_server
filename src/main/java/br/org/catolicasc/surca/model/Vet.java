a824a271dd7037146eb381b239548c688259b776
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-09-02 13:11:19 -0300

    Remove import @NotEmpty

--- src/main/java/br/org/catolicasc/surca/model/Vet.java
+++ src/main/java/br/org/catolicasc/surca/model/Vet.java
@@ -2,7 +2,6 @@ package br.org.catolicasc.surca.model;
 
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
