c00f45af9b9984f4bd1a1bad64c3cb9892aba1fe
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-09-02 16:01:38 -0300

    Add TutorEndpoint Class

---
+++ src/main/java/br/org/catolicasc/surca/endpoint/TutorEndpoint.java
@@ -0,0 +1,83 @@
+
+package br.org.catolicasc.surca.endpoint;
+
+import br.org.catolicasc.surca.model.Tutor;
+import br.org.catolicasc.surca.repository.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("v1")
+public class TutorEndpoint {
+
+    private TutorRepository tutorDao;
+
+    @Autowired
+    public TutorEndpoint(TutorRepository tutorDao) {
+        this.tutorDao = tutorDao;
+    }
+
+    @GetMapping(path = "/user/tutor")
+    public ResponseEntity<?> listAll(Pageable pageable){
+        return new ResponseEntity<>(tutorDao.findAll(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/tutor/{id}")
+    public ResponseEntity<?> getTutorById(@PathVariable("id")Long id){
+        Optional<Tutor> tutor =  tutorDao.findById(id);
+        return new ResponseEntity<>(tutor, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/tutor/nome/{nome}")
+    public ResponseEntity<?> getTutorByName(@PathVariable("nome")String name, Pageable pageable){
+        Page<Tutor> tutors =  tutorDao.findByName(pageable, name);
+        return new ResponseEntity<>(tutors, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/tutor/nome/like/{nome}")
+    public ResponseEntity<?> getTutorByNameLike(@PathVariable("nome")String name, Pageable pageable){
+        Page<Tutor> tutors =  tutorDao.findByNameStartingWith(pageable, name);
+        return new ResponseEntity<>(tutors, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/tutor/cpf/{cpf}")
+    public ResponseEntity<?> getTutorByCpf(@PathVariable("cpf")String cpf, Pageable pageable){
+        Page<Tutor> tutors =  tutorDao.findByCpf(pageable, cpf);
+        return new ResponseEntity<>(tutors, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/tutor/rg/{rg}")
+    public ResponseEntity<?> getTutorByRg(@PathVariable("rg")String rg, Pageable pageable){
+        Page<Tutor> tutors =  tutorDao.findByRg(pageable, rg);
+        return new ResponseEntity<>(tutors, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/tutor/cidade/{city}")
+    public ResponseEntity<?> getTutorByCity(@PathVariable("city")String city, Pageable pageable){
+        Page<Tutor> tutors =  tutorDao.findByCity(pageable, city);
+        return new ResponseEntity<>(tutors, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/veterinario/tutor")
+    public ResponseEntity<?> save(@RequestBody Tutor tutor){
+        return new ResponseEntity<>( tutorDao.save(tutor), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/veterinario/tutor/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        tutorDao.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @PutMapping(path = "/veterinario/tutor")
+    public ResponseEntity<?> update(@RequestBody Tutor tutor){
+        return new ResponseEntity<>(tutorDao.save(tutor), HttpStatus.OK);
+    }
+}
