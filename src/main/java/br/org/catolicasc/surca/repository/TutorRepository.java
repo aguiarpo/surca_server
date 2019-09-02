1eadb874a87152bea58364175f19a176f83a8c4c
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-09-02 16:01:47 -0300

    Add functions

--- src/main/java/br/org/catolicasc/surca/repository/TutorRepository.java
+++ src/main/java/br/org/catolicasc/surca/repository/TutorRepository.java
@@ -1,6 +1,14 @@
 package br.org.catolicasc.surca.repository;
 
 import br.org.catolicasc.surca.model.Tutor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface TutorRepository extends AuditableRepository<Tutor, Long> {
+    Page<Tutor> findByName(Pageable pageable, String name);
+    Page<Tutor> findByNameStartingWith(Pageable pageable, String name);
+    Page<Tutor> findByCpf(Pageable pageable, String cpf);
+    Tutor findByCpf(String cpf);
+    Page<Tutor> findByRg(Pageable pageable, String rg);
+    Page<Tutor> findByCity(Pageable pageable, String city);
 }
