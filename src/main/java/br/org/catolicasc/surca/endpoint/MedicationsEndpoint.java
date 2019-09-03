edbea3c1ebe0f404a71b5687699d64b28af6aee1
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-09-03 13:19:10 -0300

    Add MedicationsEndpoint Class

---
+++ src/main/java/br/org/catolicasc/surca/endpoint/MedicationsEndpoint.java
@@ -0,0 +1,55 @@
+package br.org.catolicasc.surca.endpoint;
+
+import br.org.catolicasc.surca.model.Medications;
+import br.org.catolicasc.surca.repository.MedicationsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("v1")
+public class MedicationsEndpoint {
+
+    private MedicationsRepository medicationsDao;
+
+    @Autowired
+    public MedicationsEndpoint(MedicationsRepository medicationsDao) {
+        this.medicationsDao = medicationsDao;
+    }
+
+    @GetMapping(path = "/veterinario/medicacoes")
+    public ResponseEntity<?> listAll(Pageable pageable){
+        return new ResponseEntity<>(medicationsDao.findAll(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/veterinario/medicacoes/{id}")
+    public ResponseEntity<?> getVaccineById(@PathVariable("id")Long id){
+        return new ResponseEntity<>(medicationsDao.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/veterinario/medicacoes/nome/{name}")
+    public ResponseEntity<?> getNivelUsuarioById(@PathVariable("name") String name, Pageable pageable){
+        Page<Medications> medications = medicationsDao.findByName(pageable, name);
+        return new ResponseEntity<>(medications, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/veterinario/medicacoes")
+    public ResponseEntity<?> save(@RequestBody Medications medications){
+        return new ResponseEntity<>(medicationsDao.save(medications), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/veterinario/medicacoes/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        medicationsDao.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @PutMapping(path = "/veterinario/medicacoes")
+    public ResponseEntity<?> update(@RequestBody Medications medications){
+        return new ResponseEntity<>(medicationsDao.save(medications), HttpStatus.OK);
+    }
+}
