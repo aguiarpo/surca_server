c2f47735ef894672c3088d3515352abe5e50ced2
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-09-03 17:01:46 -0300

    Update @CreateBy and @CreateData

--- src/main/java/br/org/catolicasc/surca/model/Auditable.java
+++ src/main/java/br/org/catolicasc/surca/model/Auditable.java
@@ -6,9 +6,12 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Component;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
@@ -16,12 +19,14 @@ import java.time.LocalDateTime;
 class Auditable {
     @DateTimeFormat(pattern = "dd-MM-yyyy hh:mm:ss")
     @CreatedDate
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdDate;
     @DateTimeFormat(pattern = "dd-MM-yyyy hh:mm:ss")
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
 
     @CreatedBy
+    @Column(nullable = false, updatable = false)
     private String createdBy;
     @LastModifiedBy
     private String lastModifiedBy;
