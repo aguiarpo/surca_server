3c122f5a2ecb8e4f915f77739e59f8ebb2e4db8d
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-09-03 13:17:31 -0300

    Add findByName function

--- src/main/java/br/org/catolicasc/surca/repository/MedicationsRepository.java
+++ src/main/java/br/org/catolicasc/surca/repository/MedicationsRepository.java
@@ -1,6 +1,9 @@
 package br.org.catolicasc.surca.repository;
 
 import br.org.catolicasc.surca.model.Medications;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface MedicationsRepository extends AuditableRepository<Medications, Long> {
+    Page<Medications> findByName(Pageable pageable, String name);
 }
