2f3d969af60957a302cb337f53d1101213e9ba34
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-09-03 13:00:05 -0300

    Add findByName function

--- src/main/java/br/org/catolicasc/surca/repository/VaccineRepository.java
+++ src/main/java/br/org/catolicasc/surca/repository/VaccineRepository.java
@@ -1,6 +1,9 @@
 package br.org.catolicasc.surca.repository;
 
 import br.org.catolicasc.surca.model.Vaccine;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface VaccineRepository extends AuditableRepository<Vaccine, Long> {
+    Page<Vaccine> findByName(Pageable pageable, String name);
 }
