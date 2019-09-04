38c2413904e52fb568bb61a0d6a54f1ba75b1bce
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-09-04 13:03:48 -0300

    Add Timezone America/Sao_Paulo

--- src/main/java/br/org/catolicasc/surca/SurcaApplication.java
+++ src/main/java/br/org/catolicasc/surca/SurcaApplication.java
@@ -3,8 +3,16 @@ package br.org.catolicasc.surca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SurcaApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SurcaApplication.class, args);
