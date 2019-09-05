ea1b9c3cc35d5b03aef1d508a1ad254f2798895e
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-09-05 17:16:17 -0300

    Add deleteByEmailAndPassword and findByEmailWithReturnPassword functions

--- src/main/java/br/org/catolicasc/surca/repository/UserRepository.java
+++ src/main/java/br/org/catolicasc/surca/repository/UserRepository.java
@@ -4,11 +4,21 @@ import br.org.catolicasc.surca.model.LevelsOfAccess;
 import br.org.catolicasc.surca.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
 
 public interface UserRepository extends AuditableRepository<User, Long> {
     Page<User> findByName(Pageable pageable, String name);
     Page<User> findByNameStartingWith(Pageable pageable, String name);
     Page<User> findByEmail(Pageable pageable, String name);
     User findByEmail(String name);
+    @Query(value = "delete from `user` where email = ?1 and `password` = ?2", nativeQuery = true)
+    @Modifying
+    @Transactional
+    void deleteByEmailAndPassword(String email, String password);
     Page<User> findByLevelsOfAccess(LevelsOfAccess levelsOfAccess, Pageable pageable);
+    @Query(value = "select * from `user` where email = ?1", nativeQuery = true)
+    User findByEmailWithReturnPassword(String email);
 }
