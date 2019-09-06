908903aa0e110aa61dc9eb309108f411482a94e9
aguiarpo <Sk4t3l1f342-+>
2019-09-06 15:32:45 -0300

    Add findByVetMicrochipIdOrCastratorId function

--- src/main/java/br/org/catolicasc/surca/repository/AnimalRepository.java
+++ src/main/java/br/org/catolicasc/surca/repository/AnimalRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface AnimalRepository extends AuditableRepository<Animal, Long> {
     Page<Animal> findByName(Pageable pageable, String name);
     Page<Animal> findBySpecies(Pageable pageable, String species);
     Page<Animal> findByBreed(Pageable pageable, String breed);
+    @Query(value = "SELECT * FROM animal where vet_microchip_id = ?1 or castrator_id = ?2", nativeQuery = true)
+    List<Animal> findByVetMicrochipIdOrCastratorId(Long idVet, Long idCastrator);
 
     @Query(value = "SELECT count(id) FROM animal WHERE tutor_id = :tutor_id ", nativeQuery = true)
     int findByTutorWithAnimal(@Param("tutor_id") Long tutorId);
