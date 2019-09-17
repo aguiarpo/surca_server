e62f71ec1f6bd6c1ad89ed6c2a2b35e820bfa319
aguiarpo <54685955+aguiarpo@users.noreply.github.com>
2019-09-17 13:50:00 -0300

    Add IncidentEndpoint class

---
+++ src/main/java/br/org/catolicasc/surca/endpoint/IncidentEndpoint.java
@@ -0,0 +1,57 @@
+package br.org.catolicasc.surca.endpoint;
+
+import br.org.catolicasc.surca.model.Incident;
+import br.org.catolicasc.surca.repository.IncidentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("v1")
+public class IncidentEndpoint {
+
+    private IncidentRepository incidentDao;
+
+    @Autowired
+    public IncidentEndpoint(IncidentRepository incidentDao) {
+        this.incidentDao = incidentDao;
+    }
+
+    @GetMapping(path = "/admin/incidentes")
+    public ResponseEntity<?> listAll(Pageable pageable) {
+        return new ResponseEntity<>(incidentDao.findAll(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/admin/incidentes/{id}")
+    public ResponseEntity<?> getIncidentById(@PathVariable("id")Long id){
+        return new ResponseEntity<>(incidentDao.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/admin/incidentes")
+    public ResponseEntity<?> save(@RequestBody Incident incident){
+        return new ResponseEntity<>(incidentDao.save(incident), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/admin/incidentes/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        incidentDao.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/admin/incidentes")
+    public ResponseEntity<?> deleteAll(@RequestBody List<Incident> incidents){
+        for(Incident incident : incidents) {
+            incidentDao.delete(incident);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/admin/incidentes")
+    public ResponseEntity<?> update(@RequestBody Incident incident){
+        return new ResponseEntity<>(incidentDao.save(incident), HttpStatus.OK);
+    }
+}
